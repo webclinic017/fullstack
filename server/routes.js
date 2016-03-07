9b05a3cf9d5aaa9507a91279bbfde7732c491810
wangxiaolong <491538285@qq.com>
2016-03-07 18:31:46 +0800

    ADD: 路由 404

--- server/routes.js
+++ server/routes.js
@@ -12,8 +12,7 @@ module.exports = function (app) {
     app.use('/api', require('./api'));
     
     // All undefined asset or api routes should return a 404
-    app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-        .get(errors[404]);
+    // app.route('/:url(api|auth|components|app|bower_components|assets)/*').get(errors[404]);
 
     // All other routes should redirect to the index.html
     // app.route('/*').get((req, res) => {
@@ -100,4 +99,7 @@ module.exports = function (app) {
             pageInfo: pageInfo
         });
     });
+
+
+    app.route('/*').get(errors[404]);
 };
