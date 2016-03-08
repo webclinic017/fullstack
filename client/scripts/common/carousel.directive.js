a69a455104638e22b9248b89e40a63a8f064e7f5
wangyuetong <tongtong32567@126.com>
2016-03-08 18:09:45 +0800

    ADD:首页banner第一次修改

--- client/scripts/common/carousel.directive.js
+++ client/scripts/common/carousel.directive.js
@@ -7,15 +7,52 @@
         .directive('twCarousel', twCarousel);
 
     function twCarousel($document) {
+
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
                 
-                element.hover(function () {
-                    $(this).addClass('open');
-                }, function () {
-                    $(this).removeClass('open');
+                var interval,
+                    i = 0,
+                    inLength = $('.carousel-inner div').length;
+
+
+                slideRun();
+
+                function slideRun () {
+                    interval = setInterval(slide, 2000);
+                }
+
+                $('.carousel').mouseover(function() {
+                    clearInterval(interval);
                 });
+                $('.carousel').mouseout(function() {
+                    interval = setInterval(slide, 2000);
+                });
+
+                 function slide () {
+                    var divStr = '.carousel-inner div:eq(' + i + ')';
+                    $(divStr).show().siblings().hide();
+                    var liStr = '.carousel-indicators li:eq(' + i + ')';
+                    $(liStr).addClass('current').siblings().removeClass('current');
+                    i++;
+                    if (i >= inLength) {
+                        i = 0;
+                    }
+                }
+
+                $('.carousel-indicators li').click(function() {
+                    var id = $(this).index();
+
+                    var divSlide = '.carousel-inner div:eq(' + id + ')';
+                    var liDot = '.carousel-indicators li:eq(' + id + ')';
+
+                    $(divSlide).addClass('active').siblings().removeClass('active');
+                    $(liDot).addClass('current').siblings().removeClass('current');
+
+
+                });
+ 
             }
         }
     }
