636f45e5a088de25569ca40a7efa2f12862f0e67
wangyuetong <tongtong32567@126.com>
2016-03-11 14:02:46 +0800

    MOD:更改明星高手、资产js

---
+++ client/scripts/home/index.controller.js
@@ -0,0 +1,32 @@
+;
+(function () {
+    'use strict';
+
+    angular.module('fullstackApp')
+        .controller('HomeIndexController', HomeIndexController);
+
+    HomeIndexController.$inject = ['$scope', 'product'];
+
+    function HomeIndexController($scope, product) {
+        
+        $scope.hotScale = [];   // 首页 投资者占比
+        $scope.hotProduct =[];  // 首页 热门的投资产品类型
+        $scope.starMaster =[];  // 首页 明星高手
+
+        $scope.$watch('ngRepeatFinished', function() {
+            product.getHomeHotScale().then(function (data) {
+                // $scope.hotScale = data;
+            });
+        });
+
+        product.getHomeHotProduct().then(function (data) {
+            $scope.hotProduct = data;
+            // console.info(data);
+        });
+
+        product.getHomeStarMaster().then(function (data) {
+            $scope.starMaster = data;
+            console.info(data);
+        });
+    }
+})();
