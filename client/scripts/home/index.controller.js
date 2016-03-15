3ba4c7f797f501d94780dba54e2f023dfb648550
wangyuetong <tongtong32567@126.com>
2016-03-15 18:32:00 +0800

    MOD:首页排行榜中推荐的高手

--- client/scripts/home/index.controller.js
+++ client/scripts/home/index.controller.js
@@ -5,14 +5,14 @@
     angular.module('fullstackApp')
         .controller('HomeIndexController', HomeIndexController);
 
-    HomeIndexController.$inject = ['$scope', 'product'];
+    HomeIndexController.$inject = ['$scope', 'product','ranklist'];
 
-    function HomeIndexController($scope, product) {
+    function HomeIndexController($scope, product, ranklist) {
         
         $scope.hotScale = [];   // 首页 投资者占比
         $scope.hotProduct =[];  // 首页 热门的投资产品类型
         $scope.starMaster =[];  // 首页 明星高手
-
+        $scope.homeMasters = [];// 首页 推荐的高手
         
 
         $scope.$watch('ngRepeatFinished', function() {
@@ -29,6 +29,11 @@
         product.getHomeStarMaster().then(function (data) {
             $scope.starMaster = data;
             // console.info(data);
+        });
+
+        ranklist.getMastersList().then(function (data) {
+            console.info(data);
+            $scope.homeMasters = data.data.slice(0, 5);
         });
     }
 })();
