82bbfe7f794c011b631ea77ede4feb3a205fb29e
wangyuetong <tongtong32567@126.com>
2016-03-17 16:21:41 +0800

    MOD:提现修改

--- client/scripts/asset/withdraw.controller.js
+++ client/scripts/asset/withdraw.controller.js
@@ -33,20 +33,26 @@
         $scope.showErr = showErr;
         $scope.hideErr = hideErr;
 
+
+        getCard();
+
+        // 获取可提取的最大金额
         forex.getAsset().then(function(data) {
             $scope.withdraw.maxAmount = data.data.balance;
         });
 
-        function toWithdraw(amount) {
-            if (typeof amount === 'undefined') {
-                showErr(amount);
-                return;
-            }
-
-            amount = Number(amount).toFixed(2);
-
-            // 该做这儿了
+        // 获取银行卡信息
+        function getCard() {
+            asset.getCard().then(function(data) {
+                if (data.is_succ) {
+                    $scope.withdraw.card.id = data.id;
+                    $scope.withdraw.card.number = data.card_id;
+                    $scope.withdraw.card.bank = data.bank_name;
+                    $scope.withdraw.card.address = data.bank_addr;
+                }
+            });
         }
+
 
         function hideErr(name) {
             if ($scope.frontErr[name]) {
@@ -59,5 +65,6 @@
                 $scope.frontErr[name].show = true;
             }
         }
+
     }
 })();
