8eea3408de4f1c9a7dd1acd44bc4bf576409f06c
gukw <38353285@qq.com>
2016-05-06 17:24:42 +0800

    t29优化，增加报名人数

--- client/scripts/bd/t29/t29.js
+++ client/scripts/bd/t29/t29.js
@@ -133,19 +133,3 @@
       }
 })();
 
-
-
- $(function() {
-     $(".hy-tabs-ul li").each(function(index){
-         var liCurrent = $(this);
-         var liIndex = index;
-         liCurrent.click(function(){
-             // console.info(index);
-             liCurrent.addClass("hy-tabs-ul-active").siblings().removeClass("hy-tabs-ul-active");
-             $(".hy-tabs-con .hy-tabs-con1:eq(" + liIndex +")").addClass("div-block").siblings().removeClass("div-block");
-
-         });
-     });
- });
-
-
