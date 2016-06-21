c68a89593deb21c8d4f9f41c03d0982d6809ac2c
tianqinisnan <smile241isnan@163.com>
2016-06-21 19:35:29 +0800

    MOD: mobile 定期跟单

--- client/scripts/regular/m_regular_detail.js
+++ client/scripts/regular/m_regular_detail.js
@@ -1,5 +1,26 @@
 jQuery(function($){
-	$('#signup').click(function(){
-		alert(123);
-	});
+	var $historyTab = $(".regular_detail_history_tab").find(".tab__item a");
+    var $historyImg = $(".regular_detail_history_tab").find(".regular_detail_history_tab__img");
+    var flagIndex = {};
+    var index;
+
+    for (var i=0; i<$historyTab.length; i++) {
+        flagIndex[i] = true;
+    }
+
+    $historyTab.click(function () {
+        index = $(this).parent().index();
+        if (flagIndex[index]) {
+            flagIndex[index] = false;
+            $(this).find(".fa-angle-up").css("display", "none");
+            $(this).find(".fa-angle-down").css("display", "block");
+            $(this).parent().find(".regular_detail_history_tab__img").css("display", "block");
+        } else {
+            flagIndex[index] = true;
+            $(this).find(".fa-angle-down").css("display", "none");
+            $(this).find(".fa-angle-up").css("display", "block");
+            $(this).parent().find(".regular_detail_history_tab__img").css("display", "none");
+        }
+        
+    });
 });
