9fddab3a2f6d65dc2e4babdbbef47b3a5b0bab79
pengjie <pengjie@tigerwit.com>
2016-08-02 13:43:23 +0800

    debug - check.js

--- client/scripts/bd/m7/m7.js
+++ client/scripts/bd/m7/m7.js
@@ -1,1 +1,1 @@
-var swiper = new Swiper('.swiper-container', {    slidesPerView: 1,    loop: true,    autoplay: 3000,    autoplayDisableOnInteraction: false});
+;(function(){    var swiper = new Swiper('.swiper-container', {        slidesPerView: 1,        loop: true,        autoplay: 3000,        autoplayDisableOnInteraction: false    });}());
