b15d5b42abd1259400f93de15e741b4872a9245b
pengjie <pengjie@tigerwit.com>
2016-08-02 14:39:18 +0800

    remove conflict in m7.js

--- client/scripts/bd/m7/m7.js
+++ client/scripts/bd/m7/m7.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-;(function(){    var swiper = new Swiper('.swiper-container', {        slidesPerView: 1,        loop: true,        autoplay: 3000,        autoplayDisableOnInteraction: false    });})();
-=======
-(function () {    var swiper = new Swiper('.swiper-container', {        slidesPerView: 1,        loop: true,        autoplay: 3000,        autoplayDisableOnInteraction: false    });})();
->>>>>>> c5145409bb0ccca1dff4305145dedab14f65cd28
+;(function(){    var swiper = new Swiper('.swiper-container', {        slidesPerView: 1,        loop: true,        autoplay: 3000,        autoplayDisableOnInteraction: false    });})();
