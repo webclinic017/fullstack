13610894bc451e450fa87cc06fe44929005ede17
pengjie <pengjie@tigerwit.com>
2016-08-18 18:28:11 +0800

    高手主页4.0 - 指令、结构、样式大致完成，后台数据、控制器待完善

--- client/scripts/common/scale_bar.directive.js
+++ client/scripts/common/scale_bar.directive.js
@@ -1,45 +1,70 @@
 ;
-(function() {
-    'use strict';
+(function () {
+	'use strict';
 
-    angular
-        .module('fullstackApp')
-        .directive('twScaleBar', twScaleBar);
+	angular
+		.module('fullstackApp')
+		.directive('twScaleBar', twScaleBar);
 
-    twScaleBar.$inject = [];
+	twScaleBar.$inject = [];
 
-    function twScaleBar() {
-        return {
-            restrict: 'A',
-            replace: true,
-            template: '<div class="scale_bar">'
-                        + '<span class="scale_bar__info">'
-                        + '</span>'
-                        + '<span class="scale_bar__item"></span>'
-                      + '</div>',
-            link: function (scope, element, attrs) {
-                var el = {
-                    info: element.find(".scale_bar__info"),
-                    item: element.find(".scale_bar__item")
-                }
-                var type = attrs.type;
-                // console.info(type);
+	function twScaleBar() {
+		return {
+			restrict: 'A',
+			replace: true,
+			template: '<div class="scale_bar">'
+			+ '<span class="scale_bar__item"></span>'
+			+ '<span class="scale_bar__info_multi">多单'
+			+ '<i></i>'
+			+ '</span>'
+			+ '<span class="scale_bar__info_empty">空单'
+			+ '<i></i>'
+			+ '</span>'
+			+ '<span class="scale_bar__info">'
+			+ '</span>'
+			+ '</div>',
+			link: function (scope, element, attrs) {
+				var el = {
+					info: element.find(".scale_bar__info"),
+					info_multi: element.find(".scale_bar__info_multi"),
+					info_empty: element.find(".scale_bar__info_empty"),
+					item: element.find(".scale_bar__item")
+				};
+				var type = attrs.type;
+				// console.info(type);
 
-                if (type === 'masterDetail' || type === 'investDetail') {
+				scope.$on("rendScaleBars",function(){
 
-                    el.item.width(scope.bar.scale + '%');
-                    el.info.html(scope.bar.number +' 笔 &nbsp;&nbsp;' + scope.bar.scale + '%');
-                }
+					if (type === 'masterDetail' || type === 'investDetail') {
 
-                if (type === 'spaceSidebar') {
-                    scope.$watch('personal.infoDegree', function (newVal, oldVal, scope) {
-                        if (newVal === oldVal) return;
-                        // console.info(scope.personal);
-                        el.item.width(scope.personal.infoDegree + '%');
-                        el.info.html(scope.personal.infoDegree + '%');
-                    });
-                }
-            }
-        }
-    }
+						el.item.width(scope.bar.scale + '%');
+						el.info_multi.css({
+							'left':scope.bar.scale * 2 - 38 + 'px'
+						});
+						el.info_empty.css({
+							'left':scope.bar.scale * 5.5 +  100 + 'px'
+						});
+						el.info_multi.html(scope.bar.number + ' 笔 &nbsp;&nbsp;' + scope.bar.scale + '%' + '<i></i>');
+						//el.info.html(scope.bar.number + ' 笔 &nbsp;&nbsp;' + scope.bar.scale + '%');
+					}
+
+					/*
+					 旧版本scaleBar
+					 if (type === 'masterDetail' || type === 'investDetail') {
+					 el.item.width(scope.bar.scale + '%');
+					 el.info.html(scope.bar.number +' 笔 &nbsp;&nbsp;' + scope.bar.scale + '%');
+					 }
+					 */
+					else if (type === 'spaceSidebar') {
+						scope.$watch('personal.infoDegree', function (newVal, oldVal, scope) {
+							if (newVal === oldVal) return;
+							// console.info(scope.personal);
+							el.item.width(scope.personal.infoDegree + '%');
+							el.info.html(scope.personal.infoDegree + '%');
+						});
+					}
+				});
+			}
+		}
+	}
 })();
--- client/scripts/trader/summary.controller.js
+++ client/scripts/trader/summary.controller.js
@@ -1,74 +1,169 @@
 ;
 (function () {
-    'use strict';
+	'use strict';
 
-    angular.module('fullstackApp')
-        .controller('TraderSummaryController', TraderSummaryController);
+	angular.module('fullstackApp')
+		.controller('TraderSummaryController', TraderSummaryController);
 
-    TraderSummaryController.$inject = ['$scope', '$location', '$state', 'trader', '$timeout'];
+	TraderSummaryController.$inject = ['$scope', '$location', '$state', 'trader', '$timeout'];
 
-    function TraderSummaryController($scope, $location, $state, trader, $timeout) {
-        $scope.summary = {};
-        var usercode;
-        var absUrl = $location.absUrl();
-        var regUsercode = /trader\/(\d+)\/#/;
+	function TraderSummaryController($scope, $location, $state, trader, $timeout) {
+		$scope.summary = {};
+		var usercode;
+		var absUrl = $location.absUrl();
+		var regUsercode = /trader\/(\d+)\/#/;
 
-        usercode = absUrl.match(regUsercode)[1];
+		usercode = absUrl.match(regUsercode)[1];
 
-        getMasterSummary(usercode);
-        getMasterProfitLine(usercode);
-        getMasterBarChart(usercode);
+		getMasterSummary(usercode);
+		getMasterProfitLine(usercode);
+		getMasterBarChart(usercode);
 
-        function getMasterSummary (usercode) {
-            trader.getMasterSummary(usercode).then(function (data) {
-                // console.info(data);
-                if (data.is_succ) {
-                    $scope.summary = data.data;
-                }
+		////////////////////////////////////////////////
+		rendColumnChart();
+		function rendColumnChart() {
+			/*调接口获取数据*/
+			$scope.testdata = [
+				{
+					year: "2016年",
+					data: [6, 1, -6, 3, 4, -5, 6, -2, 5, 4, 2, -4]
+				},
+				{
+					year: "2015年",
+					data: [0, 1, -6, 3, 4, 0, 6, 0, 5, 4, 2, -4]
+				},
+				{
+					year: "2014年",
+					data: [-50, 1, -6, 3, 4, -5, 6, -2, 5, 4, 2, -4]
+				}
+			];
 
-                // 饼图绘制
-                broadcastPieData();
+			/*让下拉框默认选中*/
+			$scope.selected = $scope.testdata[0];
 
-                function broadcastPieData () {
-                    var profitSum = [
-                        ['盈利订单',$scope.summary.order_count.profit_order],
-                        ['亏损订单',$scope.summary.order_count.loss_order]
-                    ];
-                    // console.info(profitSum);
-                    $scope.$broadcast('paintPieChart', profitSum);
-                }
-            });
-        }
+			$timeout(function () {
+				$scope.$broadcast('rendColumnData', $scope.testdata[0].data);
+			}, 1000);
 
-        function getMasterProfitLine (usercode) {
-            trader.getMasterProfitLine(usercode).then(function (data) {
-                // console.info(data);
-                $scope.$broadcast('paintLineChart', data.data);
-            });
-        }
-        
-        function getMasterBarChart (usercode) {
-            trader.getMasterBarChart(usercode).then(function (data) {
-                // console.info(data);
-                $scope.bars = [];
-                var symbolBar = {};
+			$scope.changeYear = function (year) {
+				$scope.$broadcast('rendColumnData', year.data);
+			};
 
-                $scope.$broadcast('hideLoadingImg');
+			/*渲染表格*/
+			var data = $scope.testdata;
+		}
 
-                if (data.data_num.length <= 0) return;
-                
-                angular.forEach(data.data, function(data,index,array){
-                    symbolBar[index] = {};
-                    symbolBar[index]["symbol_mod"] = index;
-                    symbolBar[index]["scale"] = data;
-                });
-                angular.forEach(data.data_num, function(data,index,array){
-                    symbolBar[index]["number"] = data;
-                });
-                angular.forEach(symbolBar, function(data,index,array){
-                    $scope.bars.push(data);
-                });
-            });
-        }
-    }
+		function getYearAverage() {
+			/*请求数据*/
+			//todo 接口服务待加入
+			trader.getYearAverage(usercode).then(function (data) {
+				if (data.is_succ) {
+					$scope.YearAverage = data.data;
+				}
+
+				//绘制柱形图
+
+			});
+		}
+
+		/*-----------------------区域图------------------*/
+		//function getAreaSummary(usercode) {
+		//    trader.getAreaData(usercode).then(function(data){
+		//
+		//    });
+		//}
+
+		$scope.areaData = [
+			0.7921, 0.7922, 0.7934, 0.7918, 0.7915, 0.787, 0.7861, 0.7861, 0.7853, 0.7867,
+			0.7827, 0.7834, 0.7766, 0.7751, 0.7739, 0.7767, 0.7802, 0.7788, 0.7828, 0.7816,
+			0.7829, 0.783, 0.7829, 0.7781, 0.7811, 0.7831, 0.7826, 0.7855, 0.7855, 0.7845
+		];
+
+		$timeout(function () {
+			$scope.$broadcast('rendAreaData', $scope.areaData);
+		}, 1000);
+
+		/*-----------------------BarChart------------------*/
+		$scope.barData = [{
+			name: 'John',
+			data: [5]
+		}, {
+			name: 'Jane',
+			data: [2]
+		}, {
+			name: 'Joe',
+			data: [3]
+		}, {
+			name: 'mali',
+			data: [6]
+		}];
+
+		$timeout(function () {
+			$scope.$broadcast('rendBarData', $scope.barData);
+		}, 1000);
+
+		$scope.changeYearType = function () {
+			$scope.$broadcast('rendBarData', $scope.barData);
+			$scope.$broadcast('rendScaleBars', $scope.bars);
+		};
+
+////////////////////////////////////////////////////////////////////////
+		function getMasterSummary(usercode) {
+			trader.getMasterSummary(usercode).then(function (data) {
+				// console.info(data);
+				if (data.is_succ) {
+					$scope.summary = data.data;
+				}
+
+				// 饼图绘制
+				broadcastPieData();
+
+				function broadcastPieData() {
+					var profitSum = [
+						['盈利订单', $scope.summary.order_count.profit_order],
+						['亏损订单', $scope.summary.order_count.loss_order]
+					];
+					// console.info(profitSum);
+					$scope.$broadcast('paintPieChart', profitSum);
+				}
+			});
+		}
+
+		function getMasterProfitLine(usercode) {
+			trader.getMasterProfitLine(usercode).then(function (data) {
+				// console.info(data);
+				$scope.$broadcast('paintLineChart', data.data);
+			});
+		}
+
+		function getMasterBarChart(usercode) {
+			trader.getMasterBarChart(usercode).then(function (data) {
+				// console.info(data);
+				$scope.bars = [];
+				var symbolBar = {};
+
+				$scope.$broadcast('hideLoadingImg');
+
+				if (data.data_num.length <= 0) return;
+
+				angular.forEach(data.data, function (data, index, array) {
+					symbolBar[index] = {};
+					symbolBar[index]["symbol_mod"] = index;
+					symbolBar[index]["scale"] = data;
+				});
+
+				angular.forEach(data.data_num, function (data, index, array) {
+					symbolBar[index]["number"] = data;
+				});
+
+				angular.forEach(symbolBar, function (data, index, array) {
+					$scope.bars.push(data);
+				});
+
+				$timeout(function () {
+					$scope.$broadcast('rendScaleBars', $scope.bars);
+				});
+			});
+		}
+	}
 })();
