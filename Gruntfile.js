2cbb17881ffa9ef942a3fbe4de75c5a761f96237
pengjie <pengjie@tigerwit.com>
2016-09-20 11:58:19 +0800

    DEBUG:文件编码引起的build_err

--- Gruntfile.js
+++ Gruntfile.js
@@ -462,6 +462,7 @@ module.exports = function (grunt) {
         var cont = fs.readFileSync(location + '_variables_'+ param +'.scss', 'utf8');
         // console.info("tiger", cont);
         fs.writeFileSync(location + '_variables.scss', cont, 'utf8');
+        console.log('whiteLabel task finish');
         grunt.task.run(['serve-ing']);
     });
 
@@ -477,6 +478,7 @@ module.exports = function (grunt) {
         var cont = fs.readFileSync(location + '_variables_'+ param +'.scss', 'utf8');
         // console.info("tiger", cont);
         fs.writeFileSync(location + '_variables.scss', cont, 'utf8');
+        console.log('whiteLabel task finish');
         grunt.task.run(['build-ing']);
     });
 
