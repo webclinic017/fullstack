36a2db874e6be6063de112eebcbf3d26f5da2058
pengjie <pengjie@tigerwit.com>
2016-10-17 18:45:39 +0800

    MOD:t31_game_ready_to_on_line

--- client/scripts/bd/t31/t31_game_wxConfig.js
+++ client/scripts/bd/t31/t31_game_wxConfig.js
@@ -8,11 +8,12 @@
     window.wx_game.userInfoHasLoaded = false;
     window.wx_game.awards = "";
     window.wx_game.game_can_start = false;
+    window.wx_game.hit_num = 0;
     //检测微信环境
     window.wx_game.isWeixin = navigator.userAgent.toLowerCase().indexOf('micromessenger') != -1;
 
     if (!window.wx_game.isWeixin) {
-        console.log("当前不是微信环境");
+        console.log("当前环境为普通浏览器");
         //干掉登录按钮
         $("#wxlogin").css("display", "none");
         //改变start按钮样式
@@ -49,53 +50,47 @@
 
                     window.wx_game.userInfoHasLoaded = true;
                     window.wx_game.game_can_start = true;
-                    window.wx_game.hit_num = info.hit;
+                    window.wx_game.hit_num = info.hit || 0;
 
                     //填入用户信息
                     //设置头像
                     document.getElementById("user_avator").src = info.headimgurl;
-
                     //昵称
                     $("#username").html(info.nickname);
-
                     //历史最好成绩
                     $("#selfBst").html(info.best_result + "''");
-
                     $("#user_bst_score").html(info.best_result + "''");
-
                     //当前排名
                     $("#curRange").html(info.rank);
-
                     var rank = info.rank;
 
                     //更改奖品图标
                     if (rank == 1) {
-                        $("#awardPic").addClass("iphone");
-                        window.wx_game.awards = "iPhone 7"
+                        $("#awardPic").attr("class","");
+                        $("#awardPic").addClass("gameData_awardPic iphone");
+                        window.wx_game.awards = "获得了iPhone 7"
                     } else if (rank > 1 && rank <= 10) {
-                        $("#awardPic").addClass("power");
-                        window.wx_game.awards = "美国队长移动电源"
+                        $("#awardPic").attr("class","");
+                        $("#awardPic").addClass("gameData_awardPic power");
+                        window.wx_game.awards = "获得了美国队长移动电源"
                     } else {
-                        $("#awardPic").addClass("vip");
-                        window.wx_game.awards = "赠送爱奇艺3个月会员资格"
+                        $("#awardPic").attr("class","");
+                        $("#awardPic").addClass("gameData_awardPic vip");
+                        window.wx_game.awards = "系统随机赠送爱奇艺3个月会员资格"
                     }
-
-                    //触发游戏开始
-                    //$("#start").trigger("start_game");
 
                 } else {
                     $("#land-tips").css("display", "block").html(data.error_msg + "<br/>即将刷新");
-
                     setTimeout(function () {
                         window.location.href = window.location.href;
                     }, 1500);
                 }
             }
         });
+
     } else {
         console.log("用户还没有登录...")
     }
-
 
     $(function () {
         $.ajax({
@@ -107,7 +102,7 @@
             success: function (data) {
                 var json = JSON.parse(data).data;
                 wx.config({
-                    debug: true,
+                    debug: false,
                     appId: json.appId,
                     timestamp: json.timestamp,
                     nonceStr: json.nonceStr,
@@ -115,8 +110,7 @@
                     jsApiList: [
                         'onMenuShareTimeline',
                         'onMenuShareAppMessage',
-                        'onMenuShareQQ',
-                        'scanQRCode'
+                        'onMenuShareQQ'
                     ]
                 });
             }
@@ -125,14 +119,13 @@
 
     wx.ready(function () {
         //console.log("wx_is_ready and hitDone event is binded");
-
-        console.log("hitDone is loadding");
+        console.log("wechat share is ready");
         //获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
         wx.onMenuShareAppMessage({
-            title: "我在“微盘大师”中击败了" + window.wx_game.hit_num + "%的用户，获得了" + window.wx_game.awards + "。哇咔咔！！！",
-            desc: "快来比拼吧",
+            title: "我在“微盘大师”中击败了" + window.wx_game.hit_num + "%的用户，" + window.wx_game.awards + "。哇咔咔！！！",
+            desc: "厉害了word微盘大师！",
             link: window.location.href,
-            imgUrl: "/activity/t31_game/share_pic.jpg",
+            imgUrl: window.location.protocol + "//" + window.location.hostname + "/activity/t31_game/share_pic_other.jpg",
             trigger: function (res) {
             },
             success: function (res) {
@@ -145,10 +138,10 @@
 
         //获取“分享给朋友”按钮点击状态及自定义分享内容接口
         wx.onMenuShareTimeline({
-            title: "我在“微盘大师”中击败了" + window.wx_game.hit_num + "%的用户，获得了" + window.wx_game.awards + "。哇咔咔！！！",
-            desc: "快来比拼吧",
+            title: "厉害了word微盘大师！",
+            desc: "我在“微盘大师”中击败了" + window.wx_game.hit_num + "%的用户，" + window.wx_game.awards + "。哇咔咔！！！",
             link: window.location.href,
-            imgUrl: "/activity/t31_game/share_pic_other.jpg",
+            imgUrl: window.location.protocol + "//" + window.location.hostname + "/activity/t31_game/share_pic.jpg",
             trigger: function (res) {
 
             },
@@ -163,10 +156,10 @@
         });
 
         wx.onMenuShareQQ({
-            title: "我在“微盘大师”中击败了" + window.wx_game.hit_num + "%的用户，获得了" + window.wx_game.awards + "。哇咔咔！！！",
-            desc: "快来比拼吧",
+            title: "厉害了word微盘大师！",
+            desc: "我在“微盘大师”中击败了" + window.wx_game.hit_num + "%的用户，" + window.wx_game.awards + "。哇咔咔！！！",
             link: window.location.href,
-            imgUrl: "/activity/t31_game/share_pic_other.jpg",
+            imgUrl: window.location.protocol + "//" + window.location.hostname + "/activity/t31_game/share_pic.jpg",
             trigger: function (res) {
             },
             complete: function (res) {
