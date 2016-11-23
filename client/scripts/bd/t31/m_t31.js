2fba6b83b5e4c6d486ef93aa0439af19abce02cd
pengjie <pengjie@tigerwit.com>
2016-11-23 10:23:54 +0800

    MOD:m_t32路由地址，帮助文档搜索功能优化

--- client/scripts/bd/t31/m_t31.js
+++ client/scripts/bd/t31/m_t31.js
@@ -5,7 +5,6 @@
     var $btn = $(".m_t31__attention .btn");
     var $detail = $("#attention_detail");
     $btn.on("touchend",function () {
-        console.log($detail[0].style.display == "block");
         $detail[0].style.display == "block"
             ? $detail.stop().slideUp(250)
             : $detail.stop().slideDown(300);
