89404ad581c72a4ac0ab9e7fd60c750d4907258e
tianqinisnan <smile241isnan@163.com>
2017-01-09 14:33:00 +0800

    MOD:vue-info

--- client/scripts/m_vue/resource/resource.js
+++ client/scripts/m_vue/resource/resource.js
@@ -1,8 +1,10 @@
 // var rootUrl = 'https://www.tigerwit.com';
 var rootUrl = '';
 var apiUrl = {
-    changePwdApi: rootUrl + '/action/public/v4/change_password'
+    changePwdApi: rootUrl + '/action/public/v4/change_password',
+    getSettingInfoApi: rootUrl + '/action/public/v4/get_info',
 };
 var apiUrlResource = {
-    changePwd:  Vue.resource(apiUrl.changePwdApi)
+    changePwd:  Vue.resource(apiUrl.changePwdApi),
+    getSettingInfo: Vue.resource(apiUrl.getSettingInfoApi)
 };
