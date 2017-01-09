43227107cee83180b8f50d39d75dc327edb5359f
pengjie <pengjie@tigerwit.com>
2017-01-09 10:45:16 +0800

    mod:vue-wap-phone

--- client/scripts/m_vue/m_vue_phone.js
+++ client/scripts/m_vue/m_vue_phone.js
@@ -3,8 +3,84 @@ if ($(".m_vue").attr("data-page") === "phone") {
     var phoneApp = new Vue({
         el: '#m_vue_phone',
         data: {
-            phone: 18811231234
-        }
+            phone: '',
+            code: '',
+            frontErr: {
+                phone: {
+                    show: false,
+                    msg: ''
+                },
+            },
+            backErr: {
+                show: false,
+                msg: ''
+            },
+            clickable:{
+                submit:true
+            }
+        },
+        methods: {
+            loginMethods: function () {
+                var self = this;
+
+                self.$validate(true, function () {
+                    /*如果所有条件都是false*/
+                    if (!self.$validatorMethod.invalid) {
+                        self.clickable.submit = false;
+                        // var test = self.$resource('/action/public/v4/change_password');
+
+                        apiUrlResource.changePwd.save({
+                            origin_pwd: self.password.pwdOld,
+                            new_pwd: self.password.pwdNew
+                        }).then(function (response) {
+                            self.clickable.submit = true;
+                            var data = JSON.parse(response.data);
+
+                            if (data.is_succ) {
+                                layer.open({
+                                    content: '修改成功',
+                                    skin: 'msg',
+                                    time: 2 //2秒后自动关闭
+                                });
+                            } else {
+                                self.backErr = {
+                                    show: true,
+                                    msg: data.error_msg
+                                };
+
+                                setTimeout(function () {
+                                    self.backErr = {
+                                        show: false,
+                                        msg: ''
+                                    };
+                                }, 3000);
+                            }
+                        }, function (error) {
+                            self.clickable.submit = true;
+                            console.log(error);
+                            alert("请求失败");
+                        });
+                    } else {
+                        self.showFrontErr('pwdOld');
+                        self.showFrontErr('pwdNew');
+                        self.showFrontErr('pwdConfirm');
+                    }
+                });
+            },
+            showFrontErr: function (name) {
+                var self = this;
+                if (self.frontErr[name]) {
+                    self.frontErr[name]["show"] = true;
+                }
+            },
+            hideFrontErr: function (name) {
+                var self = this;
+
+                if (self.frontErr[name]) {
+                    self.frontErr[name]["show"] = false;
+                }
+            }
+        },
     });
 
 }
