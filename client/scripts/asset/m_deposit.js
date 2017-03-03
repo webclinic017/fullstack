78157eed7a6b1fdf20f2b23e619116d564686164
tianqinisnan <smile241isnan@163.com>
2017-03-03 15:21:25 +0800

    MOD:H5 pay test

--- client/scripts/asset/m_deposit.js
+++ client/scripts/asset/m_deposit.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var $payList = $("#m_deposit_pay");
-
-    var order_no;
+    var $payBtn = $("#m_deposit_test a");
+    var order_no, real_id;
 
     var $pay = $payList.find("li a");
     var search = location.search;
@@ -14,6 +14,28 @@ $(document).ready(function () {
     }
     // console.info(urlPath);
 
+    $.get('/action/public/v4/get_info').then(function (data) {
+        data = JSON.parse(data);
+        console.log(data);
+        real_id = data.data.real_id;
+    });
+
+    $payBtn.on('click', function () {
+        var num = $("#m_deposit_test .number").val();
+        if (real_id) {
+            $.get('/action/public/v4/pay', {
+                mt4_id: real_id,
+                amount: num
+            }).then(function (data) {
+                data = JSON.parse(data);
+                order_no = data.data.order_no;
+                if (order_no) {
+                    window.open('/m/deposit/pay?order_no='+order_no);
+                }
+            });
+        }
+    });
+
     if (search) {
         order_no = doSearch(search, 'order_no');
         // console.info(order_no);
@@ -23,14 +45,14 @@ $(document).ready(function () {
         });
     }
 
-    $pay.on('click', function () {
-        var action_address = $(this).attr("href");
-        callNative({
-            type: "openUrl",
-            url: action_address
-        });
-        return false;
-    });
+    // $pay.on('click', function () {
+    //     var action_address = $(this).attr("href");
+    //     callNative({
+    //         type: "openUrl",
+    //         url: action_address
+    //     });
+    //     return false;
+    // });
 
     function doSearch (str, i) {
         var arr = str.substring(1).split('&');
