e88952cf7d40cb2d645b2da29674cf84906b964b
tianqinisnan <smile241isnan@163.com>
2017-03-14 11:03:02 +0800

    MOD:redbag web活动页

--- client/scripts/bd/redbag/redbag.js
+++ client/scripts/bd/redbag/redbag.js
@@ -1,5 +1,11 @@
 ;
 (function () {
+    var isLogin = false;
+    var $sectionBtn = $('.section_btn');
+    var $remark = $(".section1 .remark");
+    var $static = $('.section1 .static');
+    var $tipBtn = $static.find(".tip");
+
     $('#dowebok').fullpage({
         navigation: true,
         navigationColor: "#fff",
@@ -15,5 +21,46 @@
             },
             opacity: [0, 1]
         },
+        // afterLoad: function (link, index) {
+        //     console.log(link, index);
+        //     if (index === 1) {
+        //         activePage();
+        //     }
+        // }
     });
+    setTimeout(function () {
+        activePage();
+    }, 50);
+
+    var userCode = $.cookie('user_code');
+    // console.log(userCode);
+    if (userCode) {
+        isLogin = true;
+        $sectionBtn.find("span").html("马上查看红包");
+    }
+
+    $remark.on('click', function (event) {
+        event.stopPropagation();
+    });
+    $tipBtn.on('click', function (event) {
+        event.stopPropagation();
+        $static.addClass('animated fadeOut');
+        $remark.css({display: 'block'});
+    });
+
+    $('.section1').on('click', function () {
+        $remark.css({display: 'none'});
+        $static.removeClass('animated fadeOut');
+    });
+
+    $('.section_btn').on('click', function (e) {
+        if (!isLogin) {
+            $.fn.fullpage.moveTo(5);
+            return false;
+        }
+    });
+
+    function activePage () {
+        $static.find(".title").addClass("animated bounceIn");
+    }
 }());
