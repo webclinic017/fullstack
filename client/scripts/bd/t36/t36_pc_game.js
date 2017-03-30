76e272ac3c665e37623c96316339eadc9cbffcf3
pengjie <pengjie@tigerwit.com>
2017-03-30 11:18:26 +0800

    MOD:ALERTER-EFECT

--- client/scripts/bd/t36/t36_pc_game.js
+++ client/scripts/bd/t36/t36_pc_game.js
@@ -10,19 +10,53 @@
         $('.showCode').on('click', function (e) {
             layerId = layer.open({
                 type: 1,
-                shade: false,
+                shade: true,
                 closeBtn: 0,
                 title: false,
                 content: $('.layer_content').html(),
-                style:"background:rgba(0,0,0,0)"
+                style: "background:rgba(0,0,0,0.65)"
             });
-            $('.layui-layer').css({
-                'box-shadow':'none',
-                'background':'rgba(0,0,0,0)'
+            $('.t36_pc_layout').css({
+                '-webkit-filter': 'blur(10px)',
+                'filter': 'blur(10px)'
             })
-            $('.close_layer').click(function(){
+            $('.layui-layer-shade').css({
+                'background': 'rgba(0,0,0,0.5)'
+            })
+            $('.layui-layer').css({
+                'box-shadow': 'none',
+                'background': 'rgba(0,0,0,0)'
+            })
+            $('.close_layer').click(function () {
                 layer.closeAll();
+                $('.t36_pc_layout').css({
+                    '-webkit-filter': 'blur(0px)',
+                    'filter': 'blur(0px)'
+                })
             });
         });
+
+        // 获取中奖列表
+        $.ajax({
+            url: '/action/public/wx/winning_lists',
+            type: 'get',
+            success: function (data) {
+                var res = JSON.parse(data);
+                if (res.is_succ == true) {
+                    var list = {
+                        list: res.data
+                    };
+                    /*模板引擎*/
+                    baidu.template.LEFT_DELIMITER = '<$';
+                    baidu.template.RIGHT_DELIMITER = '$>';
+                    var list_str = baidu.template('award_table', list);
+                    $("#award_list").html(list_str);
+
+                    $('.matters').css({
+                        top: 1900 + res.data.length * 40 + 'px'
+                    })
+                }
+            },
+        })
     })
 }());
