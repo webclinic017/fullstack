9b52473d20e3cb79df11b3245b898d4c076dfcc5
pengjie <pengjie@tigerwit.com>
2017-06-19 10:46:56 +0800

    MOD:微信相关接口源变更为proxy

--- client/scripts/bd/t31/t31_game_wxConfig.js
+++ client/scripts/bd/t31/t31_game_wxConfig.js
@@ -4,7 +4,7 @@
     var wx_token = $.cookie('wx_token');
     window.wx_game = {};
     window.wx_game.login = function(){
-        var redirect_uri = window.location.protocol + "//" + window.location.hostname + '/action/public/wechat?redirect_url=' + encodeURIComponent(window.location.href);
+        var redirect_uri = 'https://proxy.tigerwit.com/action/public/wechat?redirect_url=' + encodeURIComponent(window.location.href);
         var href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6bbeaa275661873a&redirect_uri=" + encodeURIComponent(redirect_uri) + "&response_type=code&scope=snsapi_userinfo&state=#wechat_redirect";
         window.location.href = href;
     };
