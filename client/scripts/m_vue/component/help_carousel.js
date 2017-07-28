b68ac8a192b3a4fc5b4c82e1c0e96cd85796ad42
pengjie <pengjie@tigerwit.com>
2017-07-28 17:35:50 +0800

    MOD:帮助中心完成

--- client/scripts/m_vue/component/help_carousel.js
+++ client/scripts/m_vue/component/help_carousel.js
@@ -1,5 +1,5 @@
 /**
- *  通过 Vue.componet 全局注册一carousel生成组件
+ *  通过 Vue.componet 全局注册一 help-carousel组件
  *  接收一个数组用于渲染当前教程的步骤
  */
 Vue.component('help-carousel', {
@@ -17,7 +17,14 @@ Vue.component('help-carousel', {
                 '<div class="swiper-wrapper">' +
                     '<div class="swiper-slide" v-for="step in steps" v-cloak>' +
                         '<div class="swiper-slide-content">' +
-                            '<div class="swiper-slide-content__img_viewer">' +
+                            // 当图片数量大于1时 阻止冒泡，让滑动事件不会触发swiper的滑动，让当前图片可滚动
+                            '<div class="swiper-slide-content__img_viewer" v-on:touchstart.stop v-if="step.imgSrc.length > 1">' +
+                                '<p class="swiper-slide-content__img_viewer-img_box clearfix num{{ step.imgSrc.length }}">' +
+                                    '<img v-for="src in step.imgSrc" src="/images/mobile/{{ src }}.png" alt="" class="swiper-slide-content__img_box-img">' +
+                                '</p>' +
+                            '</div>' +
+                            // 当图片数量大于1时 开启冒泡
+                            '<div class="swiper-slide-content__img_viewer" v-if="step.imgSrc.length == 1">' +
                                 '<p class="swiper-slide-content__img_viewer-img_box clearfix num{{ step.imgSrc.length }}">' +
                                     '<img v-for="src in step.imgSrc" src="/images/mobile/{{ src }}.png" alt="" class="swiper-slide-content__img_box-img">' +
                                 '</p>' +
@@ -33,16 +40,21 @@ Vue.component('help-carousel', {
         '</div>'
     ,
     ready: function(){
-        console.log('help-carousel ready');
+        console.log('component help-carousel ready');
         new Swiper('.swiper-container', {
             pagination: '.swiper-pagination',
-            paginationClickable: true
+            paginationClickable: true,
+            // ios边缘拖动探测
+            iOSEdgeSwipeDetection : true
         });
     },
     data: function () {
-        console.log('help-carousel data');
+        console.log('component help-carousel data');
         return {
             countDown: this.steps,
         };
+    },
+    method: function(e){
+
     }
 });
