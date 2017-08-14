66725748e9b34d28e396371f6144a0c42073034c
pengjie <pengjie@tigerwit.com>
2017-08-14 11:42:04 +0800

    ADD:克隆网站

---
+++ client/scripts/common/cloneSite.directive.js
@@ -0,0 +1,29 @@
+/**
+ * 用于百度克隆投放网站
+ * 隐藏指令元素
+ */
+;
+(function () {
+    'use strict';
+
+    angular
+        .module('fullstackApp')
+        .directive('twCloneSite', twCloneSite);
+
+    function twCloneSite() {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                var hideHost = 'tigerwit.co.uk',
+                    showHost = 'www.tigerwit.com__demo.tigerwit.com__w.tigerwit.com__w.dev.tigerwit.com',
+                    currentHost = window.location.hostname;
+                if (hideHost.indexOf(currentHost) != -1) {
+                    element.hide();
+                }
+                else if (showHost.indexOf(currentHost) != -1) {
+                    element.show();
+                }
+            }
+        }
+    }
+})();
