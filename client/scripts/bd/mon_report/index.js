58d3c0101ec0442b5c8bd5aa3c53830e15da4019
pengjie <pengjie@tigerwit.com>
2017-09-21 09:53:56 +0800

    CHECKOUT

--- client/scripts/bd/mon_report/index.js
+++ client/scripts/bd/mon_report/index.js
@@ -2,108 +2,98 @@
 (function () {
     twH5Loader('img_detect_container', 4, allLoaded)
     function allLoaded() {
-        $(function () {
-            // 微信分享配置
-            if (wx) {
-                wxShareConfig({
-                    title: '【老虎带你炒外汇，轻松赚钱超酸爽】',
-                    subTitle: '【老虎带你炒外汇，轻松赚钱超酸爽】',
-                    shareLink: window.location.href,
-                    desc: '外汇投资选老虎，人人都是投资高手，一键跟单简单粗暴，老虎高手带你轻松赚钱！！',
-                    img: window.location.origin + '/activity/logo.png',
-                    trackPage: window.location.pathname.replace(/\//gi, ''),
-                    debug: false
-                });
-            } else {
-                console.log("微信配置错误")
-            }
-
-            $('.slide_arrow2').on('tap', function () {
-                $.fn.fullpage.moveTo(6);
-            })
-
-            function activePage(index) {
-                var sec = $('.section' + index);
-                // sec.find('.text-trans').addClass('active');
-                sec.addClass('act');
-                sec.find('.page_pic').addClass('active');
-                if (index == 5) {
-                    setTimeout(function () {
-                        sec.find('.page_pic0').addClass('active');
-                        sec.find('.page_pic1').addClass('active');
-                    }, 800);
-                }
-            }
-
-            function fadePage(index) {
-                var sec = $('.section' + index);
-                var textTrans = sec.find('.text-trans');
-                var pagePic = sec.find('.page_pic');
-                // sec.addClass('fade');
-                // sec.addClass('fade');
-
-                setTimeout(function () {
-                    sec.find('.page_pic0').removeClass('active');
-                    sec.find('.page_pic1').removeClass('active');
-                    // textTrans.removeClass('active').removeClass('fade');
-                    // pagePic.removeClass('active').removeClass('fade');
-                    sec.removeClass('act');
-                }, 500);
-            }
-
-            $('#dowebok').fullpage({
-                navigation: true,
-                navigationColor: "#fbd71f",
-                navigationPosition: "left",
-                verticalCentered: false,
-                afterRender: function () {
-                    setTimeout(function () {
-                        activePage(1);
-                    }, 200);
-                    $.fn.fullpage.moveTo(2);
-                },
-                afterLoad: function (anchorLink, index) {
-                    // debug 安卓翻页
-                    offsetAction(index);
-                    // activePage(index);
-                    if (index == 6) {
-                        $('.slide_arrow2').hide();
-                        $('.connect_us').css({
-                            right: '40%',
-                            bottom: '18%'
-                        });
-                        $('.risk_tip').addClass('active');
-                    } else {
-                        $('.slide_arrow2').show();
-                        $('.connect_us').css({
-                            right: '4%',
-                            bottom: '2%'
-                        });
-                    }
-
-                    activePage(index)
-                },
-                onLeave: function (index, nextIndex, direction) {
-                    // console.log(arguments);
-                    fadePage(index);
-                    if (direction == 'up') {
-                        setTimeout(function () {
-                            activePage(index - 1)
-                        }, 200);
-                    }
-                    else if (direction == 'down') {
-                        setTimeout(function () {
-                            activePage(index + 1)
-                        }, 200);
-                    }
-                    if (index == 6) {
-                        $('.connect_us').css({
-                            right: '4%',
-                            bottom: '2%'
-                        });
-                    }
-                }
+        // 微信分享配置
+        if (wx) {
+            wxShareConfig({
+                title: '【老虎带你炒外汇，轻松赚钱超酸爽】',
+                subTitle: '【老虎带你炒外汇，轻松赚钱超酸爽】',
+                shareLink: window.location.href,
+                desc: '外汇投资选老虎，人人都是投资高手，一键跟单简单粗暴，老虎高手带你轻松赚钱！！',
+                img: window.location.origin + '/activity/logo.png',
+                trackPage: window.location.pathname.replace(/\//gi, ''),
+                debug: false
             });
-        })
+        } else {
+            console.log("微信配置错误")
+        }
+
+        // 图标公用配置
+        var chartBaseCfg = {
+            borderColor: '#475b96',
+            borderWidth: [3, 3, 3, 3],
+            backgroundColor: ['#ffab00', '#003655', '#ff433a', '#8dc9d3'],
+
+            options: {
+                circumference: 2 * Math.PI,
+                animation: {
+                    animateRotate: true,
+                    animateScale: true,
+                }
+            }
+        }
+
+        var chartMap = {
+            pieChart: {
+                ctx: $('#pie_chart')[0].getContext("2d"),
+                config: {
+                    type: 'pie',
+                    data: {
+                        datasets: [
+                            {
+                                data: [10, 20, 30, 10],
+                                borderColor: chartBaseCfg.borderColor,
+                                borderWidth: chartBaseCfg.borderWidth,
+                                backgroundColor: chartBaseCfg.backgroundColor
+                            }
+                        ]
+                    },
+                    options: chartBaseCfg.options
+                }
+            },
+            ringChart: {
+                ctx: $('#ring_chart')[0].getContext("2d"),
+                config: {
+                    type: 'doughnut',
+                    data: {
+                        datasets: [
+                            {
+                                data: [10, 20, 30],
+                                borderColor: chartBaseCfg.borderColor,
+                                borderWidth: [2, 2, 2, 2],
+                                backgroundColor: chartBaseCfg.backgroundColor
+                            }
+                        ]
+                    },
+                    options: chartBaseCfg.options
+                }
+            },
+        }
+
+        function rendChart(ctx, option) {
+            return new Chart(ctx, option)
+        }
+
+        $('#dowebok').fullpage({
+            navigation: true,
+            navigationColor: "#475c95",
+            navigationPosition: "left",
+            verticalCentered: false,
+            afterRender: function () {
+                // $.fn.fullpage.moveTo(3);
+            },
+            afterLoad: function (anchorLink, index) {
+                // debug 安卓翻页
+                offsetAction(index);
+                if (index == 2) {
+                    rendChart(chartMap.pieChart.ctx, chartMap.pieChart.config)
+                }
+                if (index == 3) {
+                    rendChart(chartMap.ringChart.ctx, chartMap.ringChart.config)
+                }
+            },
+            onLeave: function (index, nextIndex, direction) {
+
+            }
+        });
     }
 }());
