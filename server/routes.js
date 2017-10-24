5d12ca17bda088f55274364beb3fe0819bfbf292
tianqinisnan <smile241isnan@163.com>
2017-10-24 10:56:29 +0800

    11月份活动

--- server/routes.js
+++ server/routes.js
@@ -573,13 +573,26 @@ module.exports = function (app) {
     });
 
     // 红包活动
+    // app.route('/bd/t35').get(function (req, res) {
+    //     setEnvCf(req, res);
+    //     if (COMPANY_NAME === 'tigerwit' || COMPANY_NAME === 'lonfx' || COMPANY_NAME === 'pandafx') {
+    //         if (isMobile(req)) {
+    //             res.render('bd/t38/h5.html', extendPublic({}, req))
+    //         } else {
+    //             res.render('bd/t38/web.html', extendPublic({}, req));
+    //         }
+    //     } else {
+    //         res.render('404.html', extendPublic({}, req));
+    //     }
+    // });
+    // 11月份活动
     app.route('/bd/t35').get(function (req, res) {
         setEnvCf(req, res);
-        if (COMPANY_NAME === 'tigerwit' || COMPANY_NAME === 'lonfx' || COMPANY_NAME === 'pandafx') {
+        if (COMPANY_NAME === 'tigerwit') {
             if (isMobile(req)) {
-                res.render('bd/t38/h5.html', extendPublic({}, req))
+                res.render('bd/t36/h5.html', extendPublic({}, req))
             } else {
-                res.render('bd/t38/web.html', extendPublic({}, req));
+                res.render('bd/t36/web.html', extendPublic({}, req));
             }
         } else {
             res.render('404.html', extendPublic({}, req));
