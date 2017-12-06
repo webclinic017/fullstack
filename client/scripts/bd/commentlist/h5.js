a5a337359c3304cb82a87a2e7887d7ee9af2ffcf
tianqinisnan <smile241isnan@163.com>
2017-12-06 13:40:08 +0800

    每日汇评 H5

---
+++ client/scripts/bd/commentlist/h5.js
@@ -0,0 +1,63 @@
+;(function () {
+
+    var $content = $(".m_commentlist");
+    var $load = $(".m_commentlist_load span");
+    var $detailsBtn = $(".m_commentlist__btn a");
+    var comment_list = {
+        comment_list: []
+    };
+    var page = 1;
+    var limit = 10;
+
+    $(".m_commentlist_load .load_btn").on("tap", function () {
+        getCommentList();
+        return false;
+    });
+
+    $(document).on("tap", ".m_commentlist__btn a", function () {
+        if (isInTiger()) {
+            var action_address = window.location.origin + $(this).attr("href");
+            // console.info(action_address);
+            callNative({
+                type: "openUrl",
+                url: action_address
+            });
+            return false;
+        }
+    });
+
+    getCommentList();
+    function getCommentList() {
+        $load.removeClass("active");
+        $(".m_commentlist_load .load").addClass("active");
+        var offet = (page - 1)*limit;
+        publicRequest('getCommentList', 'GET', {
+            offset: offet,
+            limit: limit
+        }).then(function (data) {
+            // console.log(data);
+            if (data.is_succ) {
+                page++;
+                comment_list = {
+                    comment_list: comment_list.comment_list.concat(data.data.comment_list)
+                };
+                insertComment();
+                $load.removeClass("active");
+
+                if (page > data.data.page_count) {
+                    $(".m_commentlist_load .load_end").addClass("active");
+                } else {
+                    $(".m_commentlist_load .load_btn").addClass("active");
+                }
+            }
+        });
+    }
+
+    function insertComment () {
+        //使用template模版
+        var html=baidu.template('template_comment_list',comment_list);
+        //渲染
+        $content.html(html);
+    }
+    
+})();
--- client/scripts/bd/common/publicAjax.js
+++ client/scripts/bd/common/publicAjax.js
@@ -23,6 +23,8 @@
         getInviteList: origin + '/user/invites', // get
         setClueCustomer: origin + '/auth/page_signup', // post
         setInviteCode: origin + '/invite_code', // post
+        //交易品种
+        getSymbols: origin + '/symbols', //get
         // 荣耀王者活动
         checkLogined: origin + '/auth/check', // get
         getRedBagPool: origin + '/bonus/index', // get
@@ -46,6 +48,11 @@
 
         // 获取交易月报
         getMonReport: origin + '/user/monthly_report',     // get
+        // 每日汇评
+        getCommentList: origin + '/comment/list',   // get
+        getCommentDetails: origin + '/comment/details',   // get
+        //挂单交易
+        setPendingTrade: origin + '/pending_trade/add',  //post
     };
 
     w.publicRequest = publicRequest;
