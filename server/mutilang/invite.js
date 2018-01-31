975931d659c663de59e5e6ed038dc510f7dd112d
pengjie <pengjie@tigerwit.com>
2018-01-31 10:51:42 +0800

    ADD:invite语言包

---
+++ server/mutilang/invite.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "mInvite01": {
+        "zh": "注册开户送红包",
+        "en": "Back to top"
+    },
+    "mInvite01": {
+        "zh": "回到顶部",
+        "en": "Back to top"
+    }
+}
