47b85730f2c62a5c1cd806e3b43949940876bb51
tianqinisnan <smile241isnan@163.com>
2018-03-07 15:39:49 +0800

    test

--- client/scripts/common/preview_image.service.js
+++ client/scripts/common/preview_image.service.js
@@ -25,27 +25,17 @@
             var div = '<div class="img_modal_wrapper"><div class="img_modal" style="top: '+offsetTop+'px;left: '+offsetLeft+'px;"><img src="'+targetUrl+'"></div></div>';
 
             $("body").append(div);
-            $('.img_modal').css({
-                position: 'fixed',
-                width: 'auto',
-                height: '10px',
-                transition: 'all .3s'
-            });
-            $('.img_modal img').css({
-                height: '100%',
-                verticalAlign: 'top'
-            });
-            console.log($('.img_modal'),$('.img_modal img').width(), $('.img_modal').width(),$('.img_modal').height(),-$('.img_modal').width()*wHeight*0.7/$('.img_modal').height()/2);
+
+            console.log($('.img_modal img')[0].naturalWidth,$('.img_modal img')[0].naturalHeight);
 
             setTimeout(function () {
-                console.log($('.img_modal'),$('.img_modal img').width(), $('.img_modal').width(),$('.img_modal').height(),-$('.img_modal').width()*wHeight*0.7/$('.img_modal').height()/2);
                 $(".img_modal").css({
                     height: '70%',
                     top: '15%',
                     left: '50%',
-                    marginLeft: -$('.img_modal').width()*wHeight*0.7/$('.img_modal').height()/2
+                    marginLeft: -$('.img_modal img')[0].naturalWidth*wHeight*0.7/$('.img_modal img')[0].naturalHeight/2
                 });
-            }, 30);
+            });
 
             $(".img_modal img").on('click', function (e) {
                 return false;
