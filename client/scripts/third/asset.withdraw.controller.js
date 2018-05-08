44eafa8fbd41f87a3a17987ddda6ed917b6afac0
tianqinisnan <smile241isnan@163.com>
2018-05-08 10:36:15 +0800

    CSE提现

--- client/scripts/third/asset.withdraw.controller.js
+++ client/scripts/third/asset.withdraw.controller.js
@@ -36,7 +36,9 @@
                 // timestamp: ,
                 // RMB:         // 折合人民币
             },
-            type: $state.params.type || 'invest',
+            type: $state.params.type || 'invest',   // invest, wallet
+            accountType: 'bank',    // bank, cse
+            cseAccount: undefined,
             success: false,
             minAmount: companyName == 'tigerwit' ? 20 : 100,
             maxAmount: 0
@@ -57,6 +59,7 @@
         $scope.openCardMdl = openCardMdl;
         $scope.openManageCardMdl = openManageCardMdl;
         $scope.changeWithdrawType = changeWithdrawType;
+        $scope.openChangeWithTypeMdl = openChangeWithTypeMdl;
 
         // 获取默认银行卡
         getCard();
@@ -347,7 +350,11 @@
                 }
 
                 function withdraw() {
-                    asset.withdraw($scope.withdraw.amount, $scope.withdraw.card.id).then(function (data) {
+                    var paramsAsset = {
+                        amount: Number($scope.withdraw.amount).toFixed(2)
+                    };
+                    $scope.withdraw.accountType === 'bank' ? paramsAsset.bank_card_id = $scope.withdraw.card.id : paramsAsset.cse_account = $scope.withdraw.cseAccount;
+                    asset.withdraw(paramsAsset).then(function (data) {
                         if (!data) return;
                         $scope.clickable = true;
 
@@ -384,6 +391,48 @@
                 } else {
                     var msg = data.message;
                     openWithdrawMdl(msg);
+                }
+            });
+        }
+        
+        function changeWithdrawAccountType (accountType) {
+            $scope.withdraw.accountType = accountType;
+        }
+
+        function openChangeWithTypeMdl () {
+            $modal.open({
+                templateUrl: '/views/asset/withdraw_dep_type_modal.html',
+                size: 'sm',
+                backdrop: 'static',
+                resolve: {
+                    passedScope: function () {
+                        return {
+                            withdrawType: $scope.withdraw.accountType
+                        };
+                    }
+                },
+                controller: function ($scope, $modalInstance, passedScope) {
+                    console.log(passedScope);
+                    $scope.withdraw = {
+                        accountType: passedScope.withdrawType
+                    };
+                    $scope.closeModal = closeModal;
+                    $scope.selectType = selectType;
+                    $scope.changeType = changeType;
+
+                    function selectType(accountType) {
+                        $scope.withdraw.accountType = accountType;
+                    }
+
+                    function changeType() {
+                        closeModal();
+                        changeWithdrawAccountType($scope.withdraw.accountType);
+                    }
+
+                    function closeModal() {
+                        $modalInstance.dismiss();
+                    }
+
                 }
             });
         }
