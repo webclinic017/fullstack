1d0db7c4b7a8a33c96162fa6d5aeb9f2bbbce9ee
tianqinisnan <smile241isnan@163.com>
2018-06-22 18:56:04 +0800

    赠金活动

--- client/scripts/bd/honor/web.js
+++ client/scripts/bd/honor/web.js
@@ -23,27 +23,23 @@
             } else {
                 $scope.bagList = [
                     {
+                        amount: 2000,
+                        front_desc_fir: "完成500手交易",
+                        front_recevie_status: "立即领取"
+                    },
+                    {
                         amount: 1000,
-                        front_desc_fir: "完成180手交易",
-                        front_desc_sec: "兑换期限120天",
+                        front_desc_fir: "完成250手交易",
                         front_recevie_status: "立即领取"
                     },
                     {
                         amount: 500,
-                        front_desc_fir: "完成90手交易",
-                        front_desc_sec: "兑换期限120天",
+                        front_desc_fir: "完成125手交易",
                         front_recevie_status: "立即领取"
                     },
                     {
                         amount: 200,
-                        front_desc_fir: "完成40手交易",
-                        front_desc_sec: "兑换期限60天",
-                        front_recevie_status: "立即领取"
-                    },
-                    {
-                        amount: 100,
-                        front_desc_fir: "完成25手交易",
-                        front_desc_sec: "兑换期限60天",
+                        front_desc_fir: "完成50手交易",
                         front_recevie_status: "立即领取"
                     }
                 ];
@@ -77,73 +73,13 @@
             }
         };
 
-        //领取手柄
-        $scope.recevieHonor = function () {
-            if ($scope.logined) {
-                layer.load(2);
-                redbag.receiveHonor().then(function (data) {
-                    layer.closeAll();
-                    if (data.is_succ) {
-                        getHonorStatus();
-                    } else {
-                        layer.msg(data.message);
-                    }
-                });
-            } else {
-                layer.msg('请先登录！');
-            }
-        };
-        //兑换手柄
-        $scope.openExchangeHonorMdl = function () {
-            $modal.open({
-                templateUrl: '/views/bd/honor/honor_modal.html',
-                size: 'sm',
-                backdrop: true,
-                controller: function ($scope, $modalInstance) {
-
-                    $scope.modal = {
-                        isComplete: false,
-                        type: undefined,
-                        message: undefined
-                    };
-                    $scope.address = "";
-
-                    $scope.closeModal = closeModal;
-                    $scope.closeModalSucc = closeModalSucc;
-
-                    $scope.exchangeHonor = function () {
-                        layer.load(2);
-                        redbag.exchangeHonor($scope.address).then(function (data) {
-                            layer.closeAll();
-                            $scope.modal.isComplete = true;
-                            if (data.is_succ) {
-                                $scope.modal.type = 'success';
-                            } else {
-                                $scope.modal.type = 'fail';
-                                $scope.modal.message = data.message;
-                            }
-                        });
-                    }
-
-                    function closeModal() {
-                        $modalInstance.dismiss();
-                    }
-                    function closeModalSucc() {
-                        getHonorStatus();
-                        $modalInstance.dismiss();
-                    }
-                }
-            });
-        };
-
         function getBagList () {
-            redbag.getRedbagPool(0, 1000, 'honorBag').then(function (data) {
+            redbag.getRedbagPool(0, 1000, 'xyzj').then(function (data) {
                 $scope.success = true;
                 // console.log(data);
                 if (data.is_succ) {
                     angular.forEach(data.data.records, function (value, index) {
-                        value.front_desc_fir = doDesc(value.pay_condition_desc).r1;
-                        value.front_desc_sec = doDesc(value.pay_condition_desc).r2;
+                        value.front_desc_fir = doDesc(value.pay_condition_desc);
                         value.front_recevie_status = "立即领取";
 
                         if (value.is_receive == 1) {
@@ -167,10 +103,7 @@
          
         function doDesc (desc) {
             var str = desc.split('(')[0];
-            return {
-                r1: str.split('，')[0],
-                r2: str.split('，')[1]
-            };
+            return str;
         }
     }
 })();
