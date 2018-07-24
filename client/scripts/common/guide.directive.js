4a2c6d9abc7ddf3c401d857d12e6651736535725
tianqinisnan <smile241isnan@163.com>
2018-07-24 17:54:56 +0800

    DEBUG

--- client/scripts/common/guide.directive.js
+++ client/scripts/common/guide.directive.js
@@ -18,23 +18,26 @@
                 guideType: '@'
             },
             template: 
-                '<div ng-if="guideShow" ng-click="nextGuide()" style="position: fixed;top: 0;left: 0;z-index: 100000;">'+
-                    '<img style="width: 100%;height: 100%;" src="{{src}}">'+
+                '<div ng-if="guideShow" ng-click="nextGuide()" style="position: fixed;top: 0;z-index: 100000;width:2560px;height:1285px;left: 50%;margin-left: -1280px;" ng-style="src">'+
                 '</div>',
             link: function (scope, element, attrs) {
               var ind = 0;
-              if (localStorage[scope.guideType]) return;
-              localStorage[scope.guideType] = true;
+            //   if (localStorage[scope.guideType]) return;
+            //   localStorage[scope.guideType] = true;
               
               if (scope.srcList.length) {
                 scope.guideShow = true;
-                scope.src = scope.srcList[ind];
+                scope.src = {
+                    background: "url("+scope.srcList[ind]+") #444 no-repeat"
+                };
               }
                 
               scope.nextGuide = function () {
                 ind++;
                 if (ind <= scope.srcList.length -1) {
-                  scope.src = scope.srcList[ind];
+                  scope.src = {
+                    background: "url("+scope.srcList[ind]+") #444 no-repeat"
+                  };
                 } else {
                   scope.guideShow = false;
                 }
