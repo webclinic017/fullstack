6059742a2461b84174020c3fd0e045cdfa25b735
tianqinisnan <smile241isnan@163.com>
2018-09-06 21:28:10 +0800

    Global 代理商

--- client/scripts/web/agent.controller.js
+++ client/scripts/web/agent.controller.js
@@ -6,19 +6,26 @@
         .module('fullstackApp')
         .controller('WebAgentController', WebAgentController);
 
-    WebAgentController.$inject = ['$scope', 'validator', '$modal', 'market', '$location'];
+    WebAgentController.$inject = ['$scope', 'validator', '$modal', 'market', 'account'];
 
-    function WebAgentController ($scope, validator, $modal, market, $location) {
+    function WebAgentController ($scope, validator, $modal, market, account) {
 
         $scope.becomeAgent = {
-            phone: undefined
+            phone: undefined,
+            name: undefined,
+            country: {
+                code: undefined,
+                name_en: undefined
+            },
+            email: undefined,
+            message: undefined
         };
         $scope.error = {
-            phone: {
-                show: false,
-                reg: validator.regType.phone.reg
-            }
+            is_succ: false,
+            message: ''
         };
+        $scope.loading = false;
+        $scope.countryList = [];
 
         $scope.hideErr = hideErr;
         $scope.showErr = showErr;
@@ -27,24 +34,56 @@
         var sources = getQueryString('q') || 1;
         var ad_position = getQueryString('w') || 3;
 
+        account.getWorlds().then(function (data) {
+            // console.log(data);
+            $scope.countryList = data.data;
+        });
+
         function submitForm () {
-            showErr('phone');
-            console.log(sources, ad_position);
+            $scope.error = {
+                is_succ: false,
+                message: ''
+            };
+            // showErr('phone');
+            // console.log(sources, ad_position);
             if ($scope.agentForm.$invalid) {
+                $scope.error = {
+                    is_succ: true,
+                    message:  $scope.lang.isEnglish() ? 'Please Fill in Name and Country' : '请填写姓名和国家'
+                };
                 return;
             }
-            
+            if (!$scope.becomeAgent.phone && !$scope.becomeAgent.email) {
+                $scope.error = {
+                    is_succ: true,
+                    message:  $scope.lang.isEnglish() ? 'You should fill in one of email address or phone number' : '邮箱和手机号必选一项填写'
+                };
+                return;
+            }
+            $scope.loading = true;
             market.checkPhone({
                 phone: $scope.becomeAgent.phone,
+                agent_name: $scope.becomeAgent.name,
+                world_code: $scope.becomeAgent.country.code,
+                email: $scope.becomeAgent.email,
+                comment: $scope.becomeAgent.message,
                 sources: sources,
                 ad_position: ad_position
             }).then(function (data) {
                 // console.info(data);
+                $scope.loading = false;
                 if (!data) return;
                 if (data.is_succ) {
-                    openSystemMdl('success');
+                    $scope.error = {
+                        is_succ: true,
+                        success: true,
+                        message: $scope.lang.isEnglish() ? 'Submit successfully!' : '信息提交成功!'
+                    };
                 } else {
-                    openSystemMdl('fail', data.message);
+                    $scope.error = {
+                        is_succ: true,
+                        message: data.message
+                    };
                 }
             });
 
