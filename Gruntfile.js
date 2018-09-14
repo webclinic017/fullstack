2e6e0a811a27dae9a55f25a90fafcb2fe86bc3ac
tianqinisnan <smile241isnan@163.com>
2018-09-14 15:44:06 +0800

    MOD

--- Gruntfile.js
+++ Gruntfile.js
@@ -269,8 +269,8 @@ module.exports = function (grunt) {
                             return url;
                         }
                         // console.log('----- grunt-cdnify maped url -----', url)
-                        return CDN_URL + url;
-                        // return url;
+                        // return CDN_URL + url;
+                        return url;
                     }
                 },
                 files: [{
@@ -486,7 +486,7 @@ module.exports = function (grunt) {
         if (node_env == "pro") node_env = "production";
         // set CDN URL
         // 通行证暂时不使用CDN 2018.05.29
-        CDN_URL = url === 'www' ? 'https://static.tigerwitfx.com' : 'https://staticdemo.tigerwitfx.com';
+        // CDN_URL = url === 'www' ? 'https://static.tigerwitfx.com' : 'https://staticdemo.tigerwitfx.com';
         var url_path, login_public_key;
 
         url_path = companyInfo[company][url]["url_path"];
