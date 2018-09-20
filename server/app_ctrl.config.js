8110cda56422de8e3ba8a27ba35770d0e7fe7c1b
tianqinisnan <smile241isnan@163.com>
2018-09-20 14:04:18 +0800

    MOD

--- server/app_ctrl.config.js
+++ server/app_ctrl.config.js
@@ -97,7 +97,7 @@
                         version_name : "V0.0.1",
                         description: "Update：\nOptimized register and login functions;\nAdd guest mode for international area user;\nOptimized user interface;\nFixed known bugs, optimized user experience.",
                         updated_description: "70% users are using new version",
-                        url: "https://static.tigerwitfx.com/downloads/apk/tigerwit_v2.3.0.apk",
+                        url: "https://static.tigerwitfx.com/downloads/apk/tigerwit_v2.2.3.apk",
                         force_update : false
                     }
                 }
@@ -116,7 +116,7 @@
                         version_name : "V0.0.1",
                         description: "本次更新：\n注册/登录功能优化；\n国际区用户增加【游客模式】；\n界面视觉优化；\n修复已知BUG，优化用户体验；",
                         updated_description: "70%的用户正在使用新版本",
-                        url: "https://static.tigerwitfx.com/downloads/apk/tigerwit_v2.3.0.apk",
+                        url: "https://static.tigerwitfx.com/downloads/apk/tigerwit_v2.2.3.apk",
                         force_update : false
                     }
                 }
