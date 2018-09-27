9cf1f0658288f54438e2a07091626467b924bdf6
tianqinisnan <smile241isnan@163.com>
2018-09-27 18:19:15 +0800

    M

--- server/third_napi.js
+++ server/third_napi.js
@@ -10,7 +10,7 @@ module.exports = function () {
     this.oPath = {
       register: urlPath + '/m/third/register',    //开户认证流程
       evidence: urlPath + '/m/deposit/evidence',   //入金凭证示例
-      login: urlPath + '/m/third/login',  //进入出入金页面前MT4登录页
+      paymentLogin: urlPath + '/m/third/paymentLogin',  //进入出入金页面前MT4登录页
       asset: urlPath + '/m/third/asset'   //出入金
     }
     
