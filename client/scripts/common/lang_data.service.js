cc3ebc4b174405abaa39a76fa66427a284f1696e
tianqinisnan <smile241isnan@163.com>
2019-01-11 17:06:49 +0800

    MOD

--- client/scripts/common/lang_data.service.js
+++ client/scripts/common/lang_data.service.js
@@ -4415,6 +4415,13 @@
                     }
                 },
                 depositWithdrawal:{
+                    bankCardTransferTip: {
+                        zh: "注意：必须使用所选择的银行卡进行转账",
+                        en: "Notice: The selected bank card must be used for transfer.",
+                        vi: "Chú ý: Thẻ ngân hàng được chọn phải được sử dụng để chuyển khoản.",
+                        "zh-Hant": "注意：必須使用所選擇的銀行卡進行轉賬",
+                        id:"Pemberitahuan. Rekening bank yang dipilih harus digunakan untuk pengiriman dana"
+                    },
                     title:{
                         zh:"充值提现",
                         en:"Deposit/Withdrawal",
