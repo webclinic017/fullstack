71fcc30999299f4228c9b147977b72ef98d4408f
zhao-bin-zero <1920746358@qq.com>
2019-02-25 16:27:28 +0800

    添加angularjs hostIsCn方法

--- client/scripts/common/lang.service.js
+++ client/scripts/common/lang.service.js
@@ -115,8 +115,10 @@
             isThird: function () {
                 return ($location.absUrl().indexOf('payment/asset') !== -1)
             },
+            // 区分大区area_id 1：中国区
             hostIsCn: function () {
-                return 'cn.tigerwit.com,cndemo.tigerwit.com,w.dev.tigerwit.com'.indexOf($location.host()) != -1;
+                return $cookies.area_id == 1;
+                // return 'cn.tigerwit.com,cndemo.tigerwit.com,w.dev.tigerwit.com'.indexOf($location.host()) != -1;
             }
         };
         return lang;
