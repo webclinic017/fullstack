ff10a7851e7feadd5e78b345566f907586113b1b
zhao-bin-zero <1920746358@qq.com>
2019-02-25 16:06:56 +0800

    添加node大区参数

--- server/lang.js
+++ server/lang.js
@@ -26,12 +26,14 @@ module.exports = function () {
         }
         for (var name in cookieList) {
             // console.info(name);
+            // 获取cookie中的lang
             if (name === 'lang') {
                 if(cookieList[name] instanceof Array){
                     languageTemp = cookieList[name][0];
                 } else {
                     languageTemp = cookieList[name];
                 }
+                break;
             }
         }
         if (!parseCookie(req.headers.cookie).lang && (req.host.indexOf('global.tigerwit.com') != -1 || req.host.indexOf('globaldemo.tigerwit.com') != -1)) {
@@ -51,6 +53,9 @@ module.exports = function () {
         this.req = req;
         this.company_name = company_name;
         this.clonedBaidu = clonedBaidu;
+        // 根据areaId大区显示({"key": "中国区","value": 1},{"key": "国际区","value": 2},{"key": "英国区","value": 3})
+        this.areaId = parseCookie(req.headers.cookie).area_id;
+        // console.log(this.areaId, 'areaId')
         // return this;         
     }
     Lang.prototype = {
