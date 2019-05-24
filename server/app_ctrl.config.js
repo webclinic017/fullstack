9dcb2ff9b5d2de1dac2d5dff42056e71d17e0acc
tianqinisnan <smile241isnan@163.com>
2019-05-24 16:49:23 +0800

    android 3.5.2

--- server/app_ctrl.config.js
+++ server/app_ctrl.config.js
@@ -94,10 +94,10 @@
                 },
                 android: {
                     app_info: {
-                        version_name : "V3.5.1",
-                        description: "This update: \n1.Optimized onboarding process, easier to register an account.\n2.Bitcoin, Ethereum, Litecoin and Ripple trading\n3.Simplified the withdrawal process\n4.Modified user interface",
+                        version_name : "V3.5.2",
+                        description: "This update: \n1. Added Portuguese\n2. Fixed known bugs\n3. Optimized details",
                         updated_description: "70% users are using new version",
-                        url: "https://static.tigerwitfx.com/downloads/apk/tigerwit_v3.5.1.apk",
+                        url: "https://static.tigerwitfx.com/downloads/apk/tigerwit_v3.5.2.apk",
                         force_update : true
                     }
                 }
@@ -113,10 +113,10 @@
                 },
                 android: {
                     app_info: {
-                        version_name : "V3.5.1",
-                        description: "本次更新：\n1.简化了注册流程\n2.添加新的交易品种\n3.简化提现流程\n4.优化了UI",
+                        version_name : "V3.5.2",
+                        description: "本次更新：\n1.增加葡萄牙语\n2.修复已知bug\n3.优化细节",
                         updated_description: "70%的用户正在使用新版本",
-                        url: "https://static.tigerwitfx.com/downloads/apk/tigerwit_v3.5.1.apk",
+                        url: "https://static.tigerwitfx.com/downloads/apk/tigerwit_v3.5.2.apk",
                         force_update : true
                     }
                 }
