c27870e106a5df0340ceefa1b52c2c3ca7d1a431
tianqinisnan <smile241isnan@163.com>
2019-07-08 18:14:42 +0800

    下载android包 demo环境二维码兼容

--- client/scripts/web/download.controller.js
+++ client/scripts/web/download.controller.js
@@ -6,10 +6,14 @@
         .module('fullstackApp')
         .controller('WebDownloadController', WebDownloadController);
 
-    WebDownloadController.$inject = ['$scope'];
+    WebDownloadController.$inject = ['$scope', '$location'];
 
-    function WebDownloadController ($scope) {
-
+    function WebDownloadController ($scope, $location) {
+        console.log($location.host());
+        $scope.androidImg = "/images/web/download_m_blank_cn_20190226.png";
+        if ($location.host() === 'globaldemo.tigerwit.com' || $location.host() === 'w.dev.tigerwit.com') {
+            $scope.androidImg = "/images/web/download_m_blank_cn_demo.png";
+        }
         // $(".img_box").qrcode({
         //     text: "http://192.168.1.61/m/blank",
         //     width: 114,
