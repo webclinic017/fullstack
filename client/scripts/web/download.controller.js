d14669c9128d440ba3b4dc5d2254d0aaa937282c
tianqinisnan <smile241isnan@163.com>
2019-08-29 10:39:57 +0800

    WEB&H5 download 页面调用统计接口 user/source

--- client/scripts/web/download.controller.js
+++ client/scripts/web/download.controller.js
@@ -6,9 +6,9 @@
         .module('fullstackApp')
         .controller('WebDownloadController', WebDownloadController);
 
-    WebDownloadController.$inject = ['$scope', '$location'];
+    WebDownloadController.$inject = ['$scope', '$location', 'invite'];
 
-    function WebDownloadController ($scope, $location) {
+    function WebDownloadController ($scope, $location, invite) {
         $scope.androidLiveImg = true;
         if ($location.host() === 'globaldemo.tigerwit.com' || $location.host() === 'w.dev.tigerwit.com') {
             $scope.androidLiveImg = false;
@@ -18,5 +18,11 @@
         //     width: 114,
         //     height: 114
         // });
+
+        var source = $scope.checkUserSource();
+
+        invite.setUserSource({
+            source: JSON.stringify(source)
+        });
     }
 })();
