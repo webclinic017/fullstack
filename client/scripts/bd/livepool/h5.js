7747243770f2cce6036d59e642c50415b50d15fd
zhao-bin-zero <1920746358@qq.com>
2019-09-29 11:36:56 +0800

    livepool

--- client/scripts/bd/livepool/h5.js
+++ client/scripts/bd/livepool/h5.js
@@ -1,11 +1,12 @@
-;(function () {
-    $(".rulesBtn").on('click',function(){
-
+; (function () {
+    $(".rulesBtn").on('click', function () {
         layer.open({
             type: 1,
             skin: "livepool",
             content: $('#rulesCon').html()
-    });
+        });
     })
-    
+    $("body").on('click', '.closed', function(){
+        layer.closeAll()
+    })
 })();
