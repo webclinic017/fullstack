39eaf55cc12f42ddc3af2e2a3afd5b1b956bbb6f
zhao-bin-zero <1920746358@qq.com>
2019-11-20 16:27:34 +0800

    es6语法去除

--- client/scripts/common/fun.service.js
+++ client/scripts/common/fun.service.js
@@ -57,7 +57,7 @@
             //调用：accAdd(arg1,arg2)
             //返回值：arg1加arg2的精确结果
             accAdd: function (arg1, arg2) {
-                let r1, r2, m;
+                var r1, r2, m;
                 try { r1 = arg1.toString().split(".")[1].length } catch (e) { r1 = 0 }
                 try { r2 = arg2.toString().split(".")[1].length } catch (e) { r2 = 0 }
                 m = Math.pow(10, Math.max(r1, r2))
@@ -68,7 +68,7 @@
             //调用：subtr(arg1,arg2)
             //返回值：arg1减arg2的精确结果
             subtr: function (arg1, arg2) {
-                let r1, r2, m, n;
+                var r1, r2, m, n;
                 try { r1 = arg1.toString().split(".")[1].length } catch (e) { r1 = 0 }
                 try { r2 = arg2.toString().split(".")[1].length } catch (e) { r2 = 0 }
                 m = Math.pow(10, Math.max(r1, r2));
@@ -82,7 +82,7 @@
             //调用：accMul(arg1,arg2)
             //返回值：arg1乘以arg2的精确结果
             accMul: function (arg1, arg2) {
-                let m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+                var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
                 try { m += s1.split(".")[1].length } catch (e) { }
                 try { m += s2.split(".")[1].length } catch (e) { }
                 return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m)
@@ -92,7 +92,7 @@
             //调用：accDiv(arg1,arg2)
             //返回值：arg1除以arg2的精确结果
             accDiv: function (arg1, arg2) {
-                let t1 = 0, t2 = 0, r1, r2;
+                var t1 = 0, t2 = 0, r1, r2;
                 try { t1 = arg1.toString().split(".")[1].length } catch (e) { }
                 try { t2 = arg2.toString().split(".")[1].length } catch (e) { }
                 r1 = Number(arg1.toString().replace(".", ""))
@@ -104,7 +104,7 @@
             //调用：accRem(arg1,arg2)
             //返回值：arg1%arg2的精确结果
             accRem: function (arg1, arg2) {
-                let t1 = 0, t2 = 0, r1, r2;
+                var t1 = 0, t2 = 0, r1, r2;
                 try { t1 = arg1.toString().split(".")[1].length } catch (e) { }
                 try { t2 = arg2.toString().split(".")[1].length } catch (e) { }
                 r1 = Number(arg1.toString().replace(".", ""))
